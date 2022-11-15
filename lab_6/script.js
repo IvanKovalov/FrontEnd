b27a080a8e5b42689874e1d080dbc4e076da9019
IvanKovalov <kovalevvana8@gmail.com>
2022-11-15 18:26:36 +0300

    lab_6 done

--- lab_6/script.js
+++ lab_6/script.js
@@ -5,6 +5,7 @@ usersDiv = document.getElementById("UsersDiv");
 let users  = [];
 
 downloadButton.onclick = () => {
+   clearUsers();
     for (usersCount = 0; usersCount < 5; usersCount++){
         fetch('https://randomuser.me/api')
         .then((response) => {
@@ -21,22 +22,30 @@ downloadButton.onclick = () => {
 
 
 function showUsers() {
+    
     usersList = document.createElement("ul");
     usersList.setAttribute("id", "UsersList");
     usersDiv.appendChild(usersList);
     users.forEach(user => {
         const userDataInnerHTML = `
-        <li >
-          <p >Cell: ${user.cell}</p>
-          <p >Country: ${user.location.country}</p>
-          <p >Postcode: ${user.location.postcode}</p>
-          <p >Email: ${user.email}</p>
+        <li class="user-item">
+        <img class="user-item__img"
+            src="${user.picture.medium}"
+            alt="user picture"
+          />
+          <p class="user-item__detail">Cell: ${user.cell}</p>
+          <p class="user-item__detail">Country: ${user.location.country}</p>
+          <p class="user-item__detail">Email: ${user.email}</p>
+          <p class="user-item__detail">Coordinates: ${user.location.coordinates.latitude}, ${user.location.coordinates.longitude}</p>
         </li>`;
-        usersList.innerHTML = userDataInnerHTML; 
+        usersList.innerHTML = userDataInnerHTML;
+        console.log(user); 
     });
+    
 }
 
 function clearUsers() {
-
+    usersDiv.innerHTML = '';
+    users = [];
 }
 
